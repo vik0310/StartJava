19c1db173d6c00628cd7ddac4ed3fdd110d2d997
vik0310 <victor.gorbushin@gmail.com>
2025-12-09 16:29:04 +0300

    п1 Переменные: примитивные типы данных

---
+++ Lesson1/VariablesTheme.java
@@ -0,0 +1,18 @@
+public class VariablesTheme {
+    public static void main(String[] args) {
+        //ВЫВОД ASCII-ГРАФИКИ
+        System.out.println("\n                     /\\");
+        System.out.println("   J    a  v     v  /  \\");
+        System.out.println("   J   a a  v   v  /_( )\\");
+        System.out.println("J  J  aaaaa  V V  /      \\");
+        System.out.println(" JJ  a     a  V  /___/\\___\\\n");
+
+        System.out.println("""
+                         /\\            
+                   J    /  \\  v     v  a
+                   J   /_( )\\  v   v  a a
+                J  J  /      \\  V V  aaaaa
+                 JJ  /___/\\___\\  V  a     a
+                """);
+    }
+}
